f03bab52c82eef7e7385b806847afc46b4a83789
Zhijun Guo <zhijguo@microsoft.com>
2023-01-06 11:10:34 -0800

    added smaple

---
+++ streamlit_sample.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.header('Hello 🌎!')
+if st.button('Balloons?'):
+    st.balloons()
+
+st.markdown("**DO YOU KNOW** You can click on the <Record a screencast> to record a video!")
